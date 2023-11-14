e9f640915f50167490e241cd8b314ab10146024d
CrystinaG <crystinagonz@gmail.com>
2023-11-13 21:34:16 -0300

    cambio variable PORT->port

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
+/*El process.env.PORT detecta la url de Vercel */
+const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs")
-app.set("views", "./src/views")
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 
-app.use(methodOverride("_method"))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 
-app.use("/", require("./src/routes/mainRoutes.js"))
+app.use("/", require("./src/routes/mainRoutes.js"));
 
-app.use("/admin", require("./src/routes/admin/productsRoutes.js"))
+app.use("/admin", require("./src/routes/admin/productsRoutes.js"));
 
-app.use("/shop", require("./src/routes/shop/shopRoutes.js"))
- 
-app.use("/auth", require("./src/routes/auth/authRoutes.js"))
+app.use("/shop", require("./src/routes/shop/shopRoutes.js"));
+
+app.use("/auth", require("./src/routes/auth/authRoutes.js"));
 
 app.use((req, res, next) => {
-  res.status(404).send("La página no existe")
+  res.status(404).send("La página no existe");
 });
 
-const PORT = 3000
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+app.listen(port, () => {
+  console.log("http://localhost:${port}");
+});
